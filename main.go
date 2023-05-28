937163ab2bb839fb1ef68ca78b67a38af7c1b60b
Cyril Kurian <cyrilkurian01@gmail.com>
2023-05-28 19:37:50 +0800

    gist id

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // landing function gives byte slice as a response body
@@ -27,13 +29,21 @@ func gistWrite(a http.ResponseWriter, b *http.Request) {
 }
 
 func gistView(a http.ResponseWriter, b *http.Request) {
+
+	gistId, err := strconv.Atoi(b.URL.Query().Get("gistid"))
+	if err != nil || gistId < 1 {
+		http.NotFound(a, b)
+		return
+	}
+
 	a.Header().Set("Content-Type", "application/json")
 	a.Header().Set("Cache-Control", "public, max-age=12345600")
 	a.Header().Add("Cache-Control", "public")
 	a.Header().Add("Cache-Control", "max-age=12345600")
 	//a.Header().Del("Cache-Control")
 	//fmt.Println(a.Header().Values("Cache-Control"))
-	a.Write([]byte(`{"ResponseKey": "This is a gist"}`))
+	//a.Write([]byte(`{"ResponseKey": "This is a gist"}`))
+	fmt.Fprint(a, "This is a gist with a specific id %d..", gistId)
 
 }
 
