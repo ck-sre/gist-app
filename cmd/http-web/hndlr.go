d4771dda6f664dd8674d6969f7e0860d77fd2ba8
Cyril Kurian <cyrilkurian01@gmail.com>
2023-10-10 22:33:27 +0800

    html post

--- cmd/http-web/hndlr.go
+++ cmd/http-web/hndlr.go
@@ -4,17 +4,13 @@ import (
 	"errors"
 	"fmt"
 	"gistapp.ck89.net/internal/dblayer"
+	"github.com/julienschmidt/httprouter"
 	"net/http"
 	"strconv"
 )
 
 // landing function gives byte slice as a response body
 func (m *mission) landing(a http.ResponseWriter, b *http.Request) {
-	if b.URL.Path != "/" {
-		m.noFound(a)
-		return
-	}
-
 	//panic("this is a panic that's deliberate")
 
 	tmplGsts, err := m.gists.Recent()
@@ -31,11 +27,12 @@ func (m *mission) landing(a http.ResponseWriter, b *http.Request) {
 }
 
 func (m *mission) gistWrite(a http.ResponseWriter, b *http.Request) {
-	if b.Method != http.MethodPost {
-		a.Header().Set("Allow", http.MethodPost)
-		m.clErr(a, http.StatusMethodNotAllowed)
-		return
-	}
+	gstData := m.newTmplData(b)
+	m.render(a, b, http.StatusOK, "write.tmpl", gstData)
+
+}
+
+func (m *mission) gistWriteNote(a http.ResponseWriter, b *http.Request) {
 
 	title := "No suspension"
 	content := "Noone wants a suspension when a new gist is created, its for the help of humanity"
@@ -49,13 +46,14 @@ func (m *mission) gistWrite(a http.ResponseWriter, b *http.Request) {
 
 	http.Redirect(a, b, fmt.Sprintf("/get?gistid=%d", gistid), http.StatusSeeOther)
 
-	a.Write([]byte(`{"Response": "Here's a new gist we're writing"}`))
+	//a.Write([]byte(`{"Response": "Here's a new gist we're writing"}`))
 
 }
 
 func (m *mission) gistView(a http.ResponseWriter, b *http.Request) {
 
-	gistId, err := strconv.Atoi(b.URL.Query().Get("gistid"))
+	args := httprouter.ParamsFromContext(b.Context())
+	gistId, err := strconv.Atoi(args.ByName("id"))
 	if err != nil || gistId < 1 {
 		m.noFound(a)
 		return
@@ -75,38 +73,6 @@ func (m *mission) gistView(a http.ResponseWriter, b *http.Request) {
 	tmplData.TmplGst = gst
 
 	m.render(a, b, http.StatusOK, "viewlayer.tmpl", tmplData)
-
-	//tmpls := []string{
-	//	"./ui/html/baselayer.tmpl",
-	//	"./ui/html/partials/redirect.tmpl",
-	//	"./ui/html/pages/viewlayer.tmpl",
-	//}
-	//
-	//pt, err := template.ParseFiles(tmpls...)
-	//if err != nil {
-	//	m.serverErr(a, b, err)
-	//	return
-	//}
-	//
-	//gstData := tmplData{
-	//	TmplGst: gst,
-	//}
-	//
-	//err = pt.ExecuteTemplate(a, "baselayer", gstData)
-	//if err != nil {
-	//	m.serverErr(a, b, err)
-	//	return
-	//}
-	//
-	//a.Header().Set("Content-Type", "application/json")
-	//a.Header().Set("Cache-Control", "public, max-age=12345600")
-	//a.Header().Add("Cache-Control", "public")
-	//a.Header().Add("Cache-Control", "max-age=12345600")
-	//a.Header()["X-XSS-Protection"] = []string{"1; mode=block"}
-	//a.Header().Del("Cache-Control")
-	//fmt.Println(a.Header().Values("Cache-Control"))
-	//a.Write([]byte(`{"ResponseKey": "This is a gist"}`))
-	//fmt.Fprintf(a, "+%v", gst)
 
 }
 
--- cmd/http-web/paths.go
+++ cmd/http-web/paths.go
@@ -10,14 +10,13 @@ func (msn *mission) paths() http.Handler {
 
 	rtr := httprouter.New()
 
-	fs := http.FileServer(http.Dir("./ui/static/"))true
-	rtr.Handler(http.MethodGet,"/static/", http.StripPrefix("/static", fs))
+	fs := http.FileServer(http.Dir("./ui/static/"))
+	rtr.Handler(http.MethodGet, "/static/", http.StripPrefix("/static", fs))
 
 	rtr.HandlerFunc(http.MethodGet, "/", msn.landing)
-	rtr.HandlerFunc(http.MethodPost, "/new", msn.gistWrite)
-	rtr.HandlerFunc(http.MethodPost, "/new", msn.gistWritePost)
+	rtr.HandlerFunc(http.MethodGet, "/new", msn.gistWrite)
+	rtr.HandlerFunc(http.MethodPost, "/new", msn.gistWriteNote)
 	rtr.HandlerFunc(http.MethodGet, "/get/:id", msn.gistView)
-
 
 	stdMid := alice.New(msn.resurrectPanic, msn.logRq, midHeaders)
 	return stdMid.Then(rtr)
